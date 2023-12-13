7fef739ab0a9452bb4503f5cdb447082796b9e08
Julio Rabadán <julio@somms.net>
2023-12-13 19:05:29 +0100

    Code cleaned for Observation Species.Added species_group filter optionSolved bug

--- src/Provider/Observation/ObservationSpecies.php
+++ src/Provider/Observation/ObservationSpecies.php
@@ -9,44 +9,12 @@ use Somms\BV2Observation\Data\Species;
 class ObservationSpecies extends RemoteSpecies
 {
 
-    /*
- * @var boolean
- */
-    protected bool $inObservation;
-
-
-    public function getInObservationOrg(): bool
-    {
-        if ($this->inObservation === null) {
-
-            $this->inObservation = false;
-
-            $URL = "https://old.observation.org/soort/autocomplete_name?q=" . urlencode($this->scientificName);
-            $doc = hQuery::fromUrl($URL, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
-
-            if ($doc->text() != '') {
-                $observationParser = new ObservationSpeciesParser();
-                $speciesNames = explode("\n", $doc->text());
-                foreach ($speciesNames as $speciesName) {
-                    $observationParser->setInput($speciesName);
-                    $observationSpecies = $observationParser->getSpecies();
-                    $this->inObservation = ($observationSpecies->getScientificName() == $this->getScientificName());
-                    if ($this->inObservation) {
-                        $this->setObservationId($observationSpecies->getObservationId());
-                        break;
-                    }
-                }
-
-            }
-
-        }
-
-        return $this->inObservation;
-    }
-
     protected function queryRemoteSource(string $speciesName, string $author, $options = []): mixed
     {
         $URL = "https://old.observation.org/soort/autocomplete_name?q=" . urlencode($this->scientificName);
+        if(isset($options['species_group'])){
+            $URL .= '&q=' . $options['species_group'];
+        }
         $doc = hQuery::fromUrl($URL, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
         if ($doc->text() != '') {
             $observationParser = new ObservationSpeciesParser();
@@ -54,9 +22,13 @@ class ObservationSpecies extends RemoteSpecies
             foreach ($speciesNames as $speciesName) {
                 $observationParser->setInput($speciesName);
                 $observationSpecies = $observationParser->getSpecies();
-                $this->inRemote = ($observationSpecies->getScientificName() == $this->getScientificName());
+                $this->inRemote = (
+                    $observationSpecies->getScientificName() == $this->getScientificName() ||
+                    $observationSpecies->getScientificName() == $this->getScientificName() . ' indet.' ||
+                    $observationSpecies->getScientificName() == $this->getScientificName() . ' spec.'
+                );
                 if ($this->inRemote) {
-                    $this->setRemoteId($observationSpecies->getRemoteId());
+                    return $speciesName;
                     break;
                 }
             }
